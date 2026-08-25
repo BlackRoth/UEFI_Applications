501d7ae8ead2c2ecaa659b6ae260ec0b463de757
BlackRoth <rohith927.kumar@gmail.com>
2026-08-25 13:58:21 +0530

    Add files via upload

--- PCI_Scanner/scanner.c
+++ PCI_Scanner/scanner.c
@@ -9,19 +9,22 @@ UefiMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable){
   UINT16 vendorid,deviceid;
   EFI_INPUT_KEY key;
   
-  SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+  SystemTable->ConOut->ClearScreen(SystemTable->ConOut); //clear screen
   
   Print(L"EDK2 BARE METAL PCI SCANNER (ALL SLOTS)\n\n");
-  for(device=0;device<32;device++){
-    address=(UINT32)((1<<31)|(device<<11));
+  for(device=0;device<32;device++){ //loop through all possible 32 hardware slots on primary bus 0
+    //construct a strict 32 bit PCI address structure
+    //enable config bit 31 and shift device index left by 11 bits to align to hardware pins
+    address=(UINT32)((1<<31)|(device<<11)); 
     
-    IoWrite32(0xCF8,address);
-    rawdata=IoRead32(0xCFC);
+    IoWrite32(0xCF8,address); //write target address to hardware configuration port 0XCF8
+    rawdata=IoRead32(0xCFC); //read from target address to data port 0xCFC
     
+    //isolating ids, lower 16 bits for vendorid and upper 16 bits for deviceid
     vendorid=(UINT16)(rawdata&0xFFFF);
     deviceid=(UINT16)(rawdata>>16);
     
-    if(vendorid!=0xFFFF){
+    if(vendorid!=0xFFFF){ //valid chip is responding if vendorid!=0xFFFF
       Print(L"Found device in slot %02d: vendor [0x%04X] | device [0x%04X]\n\n",device,vendorid,deviceid);
     }
   }
